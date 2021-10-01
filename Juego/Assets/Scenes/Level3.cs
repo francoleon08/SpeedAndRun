a52334d7f6f231df8f5df15fc7c8b77b1faba01c
francoleon08 <francoleon284@gmail.com>
2021-09-30 23:03:41 -0300

    Menu y levels 2-3

---
+++ Juego/Assets/Scenes/Level3.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(0,32f, -3.8f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
